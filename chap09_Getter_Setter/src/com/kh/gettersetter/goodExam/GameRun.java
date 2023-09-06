9b0c71790322348113f8b2e649ba7decc426a912
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-06 11:06:33 +0900

    Getter_Setter

---
+++ chap09_Getter_Setter/src/com/kh/gettersetter/goodExam/GameRun.java
@@ -0,0 +1,24 @@
+package com.kh.gettersetter.goodExam;
+
+public class GameRun {
+
+	public static void main(String[] args) {
+		// 플레이 생성자 만들기
+		GoodPlayer player1 = new GoodPlayer();
+		GoodPlayer player2 = new GoodPlayer();
+		
+		
+		player1.setName("");
+		player1.isValidName();
+		player1.setHealth(-10);
+
+
+		
+		
+	
+		
+		
+		
+	}
+
+}
