d394043dcc58b891191e805979597c7783d341c9
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-06 09:02:19 +0900

    Practice

--- Practice/src/com/kh/practice10/Practice10.java
+++ Practice/src/com/kh/practice10/Practice10.java
@@ -8,7 +8,7 @@ public class Practice10 {
 		String jumin = "123456-1234567";
 
 		//2. 성별자리 이후 별표 만드는거 만들기
-
+		String makeStar = "*";
 		
 		//3. 주민번호 성별자리 이후부터 *로 가리고 출력
 		// to char Array를 사용해서 String -> char Array 로 변경
