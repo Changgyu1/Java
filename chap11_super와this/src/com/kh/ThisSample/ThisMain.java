a0fcc0c1ce34487ef7cc35b8f64f199f2b7f5110
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-07 11:20:46 +0900

    super와this

--- chap11_super와this/src/com/kh/ThisSample/ThisMain.java
+++ chap11_super와this/src/com/kh/ThisSample/ThisMain.java
@@ -6,20 +6,18 @@ this()  현재 클래스의 다른 생성자를 호출하는데 사용
 
 class MyClass {
 	int myField; // this myField
-	int my;
 	// 첫 번째 생성자
 	MyClass() {
-		this(5); // this() 사용해서 다른 생성자 호출
+		this(0); // this() 사용해서 다른 생성자 호출
 	}
 	
 	// 두 번째 생성자
 	MyClass(int value) {
 		this.myField = value;
-		this.my = value;
 	}
 
 	void printValue() {
-		System.out.println("myField의 값 : " + this.my);
+		System.out.println("myField의 값 : " + this.myField);
 	}
 }
 
