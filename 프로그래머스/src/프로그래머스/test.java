66220cf8e8132593c9d9bc8c53726cb57cbb49ee
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-15 11:14:20 +0900

    ,

---
+++ 프로그래머스/src/프로그래머스/test.java
@@ -0,0 +1,20 @@
+package 프로그래머스;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+        int[] answer = {1,2,3,4,5};
+        int j = 0;
+        for(int a : answer){
+        	System.out.println(a+a);
+        }
+		
+		
+		
+		
+		
+	}
+
+}
