f9adb41a8028277af8461d6a91f98611fbd33fa2
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-15 17:47:47 +0900

    metadata

--- 프로그래머스/src/프로그래머스/test.java
+++ 프로그래머스/src/프로그래머스/test.java
@@ -1,20 +1,11 @@
 package 프로그래머스;
 
-import java.util.Scanner;
-
 public class test {
 
-	public static void main(String[] args) {
-        int[] answer = {1,2,3,4,5};
-        int j = 0;
-        for(int a : answer){
-        	System.out.println(a+a);
-        }
-		
-		
-		
-		
-		
-	}
-
+   public static void main(String[] args) {
+	   String message = "happy birthday!";
+	   
+	   
+	   System.out.println(message.length());
+   }
 }
