4096a0fafbc4e16c0259bb747a1b18a434fb60e7
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-18 17:03:52 +0900

    .

--- 프로그래머스/src/프로그래머스/test.java
+++ 프로그래머스/src/프로그래머스/test.java
@@ -3,9 +3,23 @@ package 프로그래머스;
 public class test {
 
    public static void main(String[] args) {
-	   String message = "happy birthday!";
+	   String[] str1 = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+	   int n = 3;
+	   int count = 0;
+	   
+	   for(int i = 0; i < str1.length; i++) {
+		   if(str1[i].charAt(str1.length-1) == str1[i+1].charAt(0)) {
+			   System.out.println("같음");
+		   }else {
+			   System.out.println("다름");
+		   }
+	   }
 	   
 	   
-	   System.out.println(message.length());
+	   
+
+	   System.out.println(str1[0].length());
+	   System.out.println(str1[0].charAt(3));
+	   
    }
 }
