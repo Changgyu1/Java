57995abe3007cb491c6d91595f448be406ffe9e1
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-19 17:32:37 +0900

    FF

--- 프로그래머스/src/프로그래머스/test.java
+++ 프로그래머스/src/프로그래머스/test.java
@@ -1,25 +1,42 @@
 package 프로그래머스;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class test {
 
    public static void main(String[] args) {
-	   String[] str1 = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
-	   int n = 3;
-	   int count = 0;
+	   int[] number = {0, 31, 24, 10, 1};
+	   int answer = 0;
+	   String rsp = "2";
+	   switch(rsp){
+       case 2:
+           answer = "0";
+           break;
+       case 0:
+           answer = "5";
+           break;
+       case 5:
+           anser ="2";
+           break;
+       default:  
+   }
+	   Arrays.sort(number); // 오름차순
 	   
-	   for(int i = 0; i < str1.length; i++) {
-		   if(str1[i].charAt(str1.length-1) == str1[i+1].charAt(0)) {
-			   System.out.println("같음");
-		   }else {
-			   System.out.println("다름");
-		   }
-	   }
+	   answer = number[number.length-2] * number[number.length-1];
+	   System.out.println(answer);
+       
+       }
+}
+	 
+
+
+       
+		
 	   
 	   
 	   
 
-	   System.out.println(str1[0].length());
-	   System.out.println(str1[0].charAt(3));
+
 	   
-   }
-}
