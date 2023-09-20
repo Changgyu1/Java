7ed8fdd4e66363a9cc4915979e344d9d72625026
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-20 17:33:44 +0900

    FF

--- 프로그래머스/src/프로그래머스/test.java
+++ 프로그래머스/src/프로그래머스/test.java
@@ -5,17 +5,9 @@ public class test {
    public static void main(String[] args) {
 	   int n = 12345;
 	   int count = 0;
-	   String str = Integer.toString(n);
-	   String[] arr = str.split(""); // 1,2,3,4,5
-	   String[] answer = new String[str.length()];
-	   
-	   for(int i = str.length()-1; i >= 0; i--) {
-		   answer[count] = arr[i];
-		   count++;
-	   }
-	   for(String a : answer) {
-		   System.out.println(a);
-       }
+	  
+	   String
+       
    }
 }
 	 
@@ -23,7 +15,7 @@ public class test {
 
        
 		
-	   
+	    
 	   
 	   
 
