5283f935067e4092836badf51c4f651792c0fc6e
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-09-26 14:03:38 +0900

    a

--- 프로그래머스/src/프로그래머스/sysou.java
+++ 프로그래머스/src/프로그래머스/sysou.java
@@ -1,47 +1,26 @@
 package 프로그래머스;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class sysou {
-
+	
 	public static void main(String[] args) {
-		boolean isTrue = false;
-		int black = 1;
-		String blank = "□";
-		String KingB = "♠";
-		String KingW = "♤";
-		char[] ah = new char[8];
+		int divisor = 5;
+		int[] arr = {3,2,6};
+		
+		int count = 0;
+		int[] answer = new int [arr.length];
+        
+        for(int a : arr){
+        	if(a % divisor == 0) {
+        		answer[count] = a;
+        		count++;
+        	}
 
-
-		String[][] che = { { blank, blank, blank, blank, blank, blank, blank, blank },
-				{ blank, blank, blank, blank, blank, KingW, blank, blank },
-				{ blank, blank, blank, blank, blank, blank, blank, blank },
-				{ blank, blank, blank, blank, blank, blank, blank, blank },
-				{ blank, blank, blank, blank, blank, blank, blank, blank },
-				{ blank, blank, blank, blank, blank, blank, blank, blank },
-				{ blank, blank, KingB, blank, blank, blank, blank, blank },
-				{ blank, blank, blank, blank, blank, blank, blank, blank } };
-
-		while (!isTrue) {
-			for (int i = 0; i < che.length; i++) {
-				for (int a = 0; a < che.length; a++) {
-					if (che[i][a].contains(KingB) && che[i][a].contains(KingW)) {
-						System.out.println(che[i][a].equals(KingB));
-						isTrue = true;
-					} else {
-						System.out.println(che[i][a].contains(KingB));
-						isTrue = true;
-					}
-				}
-			}
-			System.out.println("입력");
-			Scanner sc = new Scanner(System.in);
-			String a = sc.next();
-			System.out.println(a);
-		}
-
+        }
+        for(int s : answer) {
+        	System.out.println(s
+        			);
+        }
 	}
-
 }
